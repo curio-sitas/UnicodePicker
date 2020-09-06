633f7b700299cf30c10876cdcbd4b3763d7ea0de
Brian SINQUIN <paradisepx@gmail.com>
2020-09-06 17:51:38 +0200

    NodeJS + electron setup

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Cree la fenetre du navigateur.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // et charger le fichier index.html de l'application.
+  win.loadFile('index.html')
+
+  // Ouvre les DevTools.
+  win.webContents.openDevTools()
+  win.setMenuBarVisibility(false)
+  win.setResizable(false)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+
+  if (win === null) {
+    createWindow()
+  }
+})
+
